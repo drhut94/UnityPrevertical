4433dd81ce6a2266dc1dab18ff8b81685f54f00f
drhut94 <drhut94@gmail.com>
2019-12-16 23:20:25 +0100

    Level editor custom editor

---
+++ Prevertical/Assets/LvlEditor/LvlEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LvlEditor : MonoBehaviour
+{
+    public Vector3 lookAtPoint = Vector3.zero;
+    public bool isActive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(lookAtPoint);
+    }
+
+    public void EnableEditorMode() {
+        Debug.Log("<color=red>Editor mode is now enabled</color>");
+        isActive = true;
+    }
+
+    public void DisableEditorMode() {
+        Debug.Log("Editor mode is now disabled");
+        isActive = false;
+    }
+}
---
+++ Prevertical/Assets/LvlEditor/LvlEditorWindow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LvlEditor))]
+[CanEditMultipleObjects]
+public class LvlEditorWindow : Editor
+{
+    //SerializedProperty lookAtPoint;
+
+    // Start is called before the first frame update
+    void OnEnable() {
+        //lookAtPoint = serializedObject.FindProperty("lookAtPoint");
+    }
+
+   
+    public override void OnInspectorGUI() {
+
+        LvlEditor lvlEditor = (LvlEditor)target;
+
+        EditorGUILayout.Space(20);
+        GUI.backgroundColor = lvlEditor.isActive ? Color.red : Color.green;
+
+
+        if (GUILayout.Button(lvlEditor.isActive ? "DISABLE EDITOR MODE" : "ENABLE EDITOR MODE")) {
+            if (lvlEditor.isActive)
+                lvlEditor.DisableEditorMode();
+            else
+                lvlEditor.EnableEditorMode();
+        }
+
+        GUI.backgroundColor = Color.white;
+        EditorGUILayout.Space(20);
+
+        serializedObject.Update();
+        //EditorGUILayout.PropertyField(lookAtPoint);
+        serializedObject.ApplyModifiedProperties();
+    }
+
+
+}
